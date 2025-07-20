a1fc5f2c75f62043f4e1b60e54ac9fa34d82a345
Meng Bo <mengbo@lnu.edu.cn>
2025-07-20 19:17:47 +0800

    Fix bug on linux

--- zshrc.d/20dircolors.zsh
+++ zshrc.d/20dircolors.zsh
@@ -1,3 +1,7 @@
-eval `gdircolors ~/.dir_colors`
+if command -v gdircolors >/dev/null 2>&1; then
+  eval "$(gdircolors ~/.dir_colors)"
+else
+  eval "$(dircolors ~/.dir_colors)"
+fi
 # fix dircolors for Selenized
 export LS_COLORS="$LS_COLORS:ow=1;7;34:st=30;44:su=30;41"
