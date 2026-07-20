4c85ef33f1c289fa6b7f27c2f3d9cb0e9b600ec4
Meng Bo <mengbo@lnu.edu.cn>
2026-07-20 10:45:48 +0800

    change proxy set script

--- zshrc.d/90clash.zsh
+++ zshrc.d/90clash.zsh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 PROXY_URL="http://clash-server:7890"
+NO_PROXY_LIST="localhost,.lan,.local,127.0.0.1,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
 
 if command -v curl >/dev/null 2>&1 \
    && curl -s --connect-timeout 1 "$PROXY_URL" >/dev/null 2>&1
 then
     export http_proxy="$PROXY_URL"
     export https_proxy="$PROXY_URL"
-    export no_proxy="localhost,127.0.0.1"
+    export no_proxy="$NO_PROXY_LIST"
 else
     unset http_proxy https_proxy no_proxy
 fi
